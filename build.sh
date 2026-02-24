f4b724eacc9b2a4e5ef66993eec66126d0e17bda
Jeorshwa <crisif0405@gmail.com>
2026-02-24 14:32:36 -0500

    feat: crear superusuario automatico en deploy

--- build.sh
+++ build.sh
@@ -2,8 +2,14 @@
 set -o errexit
 
 pip install -r requirements.txt
-
-# --clear fuerza borrar y recopiar todo desde cero
 python manage.py collectstatic --no-input --clear
-
 python manage.py migrate
+
+# Crear superusuario automáticamente
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='patricia').exists():
+    User.objects.create_superuser('patricia', 'pattytutu190879@gmail.com', '40792145')
+    print('Superusuario creado')
+"
