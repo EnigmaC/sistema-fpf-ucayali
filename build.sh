1dcb94c2f35ce56e93b18272531467c6be21395f
Jeorshwa <crisif0405@gmail.com>
2026-02-24 13:56:50 -0500

    debug: verbose collectstatic

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-# Salir si hay errores
 set -o errexit
 
-# 1. Instalar librerías
 pip install -r requirements.txt
 
-# 2. Recolectar archivos estáticos (CSS, JS, Imágenes)
-python manage.py collectstatic --no-input
+# Cambia --no-input por --verbosity 2 para ver el detalle completo
+python manage.py collectstatic --no-input --verbosity 2
 
-# 3. Crear las tablas en la base de datos
 python manage.py migrate
